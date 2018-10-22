067be73bd1f045d66235589453bbe58e73f0b430
Danton <dantondelima.contato@gmail.com>
2018-10-22 13:54:02 -0300

    mudanças

--- wp-content/themes/meuTema/header.php
+++ wp-content/themes/meuTema/header.php
@@ -4,10 +4,10 @@
         <?php $raiz = get_template_directory_uri(); ?>
         <link rel="stylesheet" href="<?= $raiz?>/reset.css">
         <link rel="stylesheet" href="<?= $raiz?>/style.css">
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
-        <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
         <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.min.js"></script>
         <?php wp_head(); ?>
@@ -16,11 +16,5 @@
         
     <header>
         <a href="<?= get_site_url()?>">Início</a>
-        <?php 
-        $args = array(
-            'theme_location' => 'header-menu'
-        );
-            wp_nav_menu($args);
-        ?>
     </header>
 
--- wp-content/themes/meuTema/index.php
+++ wp-content/themes/meuTema/index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?> 
-        
+
+   <h1 class="jumbotron text-center" style="font-family:verdana; font-size:50px; border-radius:50px;">Lista de Treinamentos</h1>
 <?php 
 
     $args = array('post_type' => 'treinamento');
@@ -8,11 +9,13 @@
         while( $loop->have_posts()) :
             $loop->the_post();
 ?> 
-<div style="border: 1px solid black">
-    <a href="<?= the_permalink() ?>">
-    <?php the_post_thumbnail('thumbnail'); ?>
-    <h2><?php the_title(); ?></h2>
-    </a>
+<div style="margin-left:100px;">
+    <div class="col-lg-3">
+        <a class="btn btn-primary" role="button" href="<?= the_permalink() ?>">
+            <?php the_post_thumbnail('thumbnail'); ?>
+            <h2><?php the_title(); ?></h2>
+        </a>
+    </div>
 </div> 
 <?php            
         endwhile;
